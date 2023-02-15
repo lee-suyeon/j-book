a945371d5422c87235c1b26740782358e5fd688e
suyeon <sroovy@naver.com>
2023-02-15 22:05:00 +0900

    Highlighting errors

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ const App = () => {
   const ref = useRef<any>();
   const iframe = useRef<any>();
   const [ input, setInput ] = useState('');
-  const [ code, setCode ] = useState('');
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -25,6 +24,8 @@ const App = () => {
     if(!ref.current) {
       return;
     }
+
+    iframe.current.srcdoc = html;
     
     const env = ["process", "env", "NODE_ENV"].join(".");
     
@@ -51,7 +52,13 @@ const App = () => {
         <div id="root"></div>
         <script>
           window.addEventListener('message', (event) => {
-            eval(event.data);
+            try {
+              eval(event.data);
+            } catch(err) {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              throw err;
+            }
           }, false);
         </script>
       </body>
@@ -67,8 +74,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <pre>{code}</pre>
-      <iframe ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
+      <iframe title="code-preview" ref={iframe} sandbox="allow-scripts" srcDoc={html}/>
     </div>
   );
 };

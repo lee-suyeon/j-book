df642d8de00952e78418283ec353b52480caaefa
suyeon <sroovy@naver.com>
2023-02-19 16:00:47 +0900

    Displaying and Configuring the monaco editor

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,23 @@
+import MonacoEditor from '@monaco-editor/react';
+
+const CodeEditor = () => {
+  return (
+    <MonacoEditor 
+      theme="vs-dark"
+      height="500px"
+      language="javascript"
+      options={{
+        wordWrap: "on",
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+    />
+  ) 
+}
+
+export default CodeEditor;

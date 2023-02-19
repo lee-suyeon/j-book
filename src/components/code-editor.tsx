758c154419022430f9802cd43ea3d9ce53403eaa
suyeon <sroovy@naver.com>
2023-02-19 17:15:49 +0900

    Adding Prettier and Automatic Code Formatting

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,22 +1,51 @@
 import MonacoEditor from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+  value: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ value, onChange }) => {
+  const onEditorChange = (input: string | undefined) => {
+    onChange(input || "");
+  }
+
+  const onFormatClick = () => {
+    const formatted = prettier.format(value, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+    });
+    onChange(formatted);
+  }
+
   return (
-    <MonacoEditor 
-      theme="vs-dark"
-      height="500px"
-      language="javascript"
-      options={{
-        wordWrap: "on",
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <MonacoEditor
+        onChange={onEditorChange}
+        defaultValue=""
+        value={value}
+        theme="vs-dark"
+        height="500px"
+        language="javascript"
+        options={{
+          wordWrap: "on",
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   ) 
 }
 

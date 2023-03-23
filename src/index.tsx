a6db883587af4b320aad5623db982bce1e35e84c
suyeon <sroovy@naver.com>
2023-03-23 23:13:12 +0900

    Implementations Action Creator

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import 'bulmaswatch/darkly/bulmaswatch.min.css';
 import ReactDOM from 'react-dom/client';
-// import CodeCell from './components/code-cell';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
